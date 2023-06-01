b404793e44206818077bd9891826f9fc88e56202
Chomtana <Chomtana001@gmail.com>
2023-06-01 22:15:18 +0700

    Change approx 0.8.17 to at least 0.8.17

--- contracts/ethregistrar/AxelarProposalController.sol
+++ contracts/ethregistrar/AxelarProposalController.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ~0.8.17;
+pragma solidity ^0.8.17;
 
 import {ENS} from "../registry/ENS.sol";
 import {ITextResolver} from "../resolvers/profiles/ITextResolver.sol";
