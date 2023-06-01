78fd92efc3ca8ce3b602beaffd637bc9731dff21
Chomtana <Chomtana001@gmail.com>
2023-06-01 12:51:22 +0700

    Fix multi execution on axelar proposal

--- contracts/ethregistrar/AxelarProposalController.sol
+++ contracts/ethregistrar/AxelarProposalController.sol
@@ -5,14 +5,20 @@ import {ENS} from "../registry/ENS.sol";
 import {ITextResolver} from "../resolvers/profiles/ITextResolver.sol";
 
 interface IProposalExecutable {
-    function execute(bytes32 proposalId, bytes calldata data) external payable;
+    function execute(
+        bytes32 proposalId,
+        bytes32 conditionHash,
+        bytes calldata data
+    ) external payable;
 }
 
 error InvalidRecord(string key, string value);
 error ProposalExpired();
+error ProposalExecuted();
 
 contract AxelarProposalController {
     ENS public immutable ens;
+    mapping(bytes32 => uint256) public executedTimestamp;
 
     struct ProposalCondition {
         bytes32 node;
@@ -49,6 +55,12 @@ contract AxelarProposalController {
             );
     }
 
+    function calculateConditionHash(
+        ProposalDetail calldata proposal
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encode(proposal.conditions));
+    }
+
     event ExecuteProposal(
         bytes32 indexed proposalId,
         address indexed executor,
@@ -80,14 +92,22 @@ contract AxelarProposalController {
             }
 
             bytes32 proposalId = calculateProposalId(proposal);
+            bytes32 conditionHash = calculateConditionHash(proposal);
+
+            if (executedTimestamp[proposalId] > 0) {
+                revert ProposalExecuted();
+            }
 
             for (uint256 i; i < proposal.actions.length; ++i) {
                 ProposalAction memory action = proposal.actions[i];
                 action.target.execute{value: action.value}(
                     proposalId,
+                    conditionHash,
                     action.data
                 );
             }
+
+            executedTimestamp[proposalId] = block.timestamp;
 
             emit ExecuteProposal(proposalId, msg.sender, msg.value);
         }
